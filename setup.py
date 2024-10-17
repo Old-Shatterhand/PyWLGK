0df97997ee9ad2ea4b3f4817faacfee106e335a6
Roman Joeres <joeres.roman@web.de>
2024-10-17 19:15:32 +0200

    Version 1.0.1 to check auto uplaod

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as desc_file:
 
 setup(
     name="PyWLGK",
-    version="1.0.0",
+    version="1.0.1",
     description="Python package computing Weisfeiler-Lehman graph kernels",
     long_description=long_description,
     long_description_content_type="text/markdown",
